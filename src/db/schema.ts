773858e3232ede78d183821a0a330f13343bf217
cr3wcr3w <racelisjk@gmail.com>
2025-03-04 17:51:55 +0800

    chore: update schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,7 +5,7 @@ import { sql } from "drizzle-orm";
 export const authSchema = pgSchema("auth");
 
 export const users = authSchema.table("users", {
-    id: uuid("id").primaryKey().default(sql`gen_random_uuid()`),
+    id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
     firstName: text('first_name').notNull(),
     lastName: text('last_name').notNull(),
     email: text('email').notNull().unique(),
@@ -20,7 +20,7 @@ export const users = authSchema.table("users", {
 });
 
 export const identities = authSchema.table("identities", {
-    id: uuid("id").primaryKey().default(sql`gen_random_uuid()`),
+    id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
     phoneNumber: text('phone_number'),
     image: text('image'),
 
@@ -30,7 +30,7 @@ export const identities = authSchema.table("identities", {
 })
 
 export const sessions = authSchema.table("sessions", {
-    id: uuid("id").primaryKey().default(sql`gen_random_uuid()`),
+    id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
     // If not_after is reached, the session expires.
     notAfter: timestamp('not_after').notNull(),
     // If a refresh token is used, refresh_at is updated.
@@ -44,7 +44,7 @@ export const sessions = authSchema.table("sessions", {
 });
 
 export const refreshTokens = authSchema.table("refresh_tokens", {
-    id: uuid("id").primaryKey().default(sql`gen_random_uuid()`),
+    id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
     token: text("token").notNull().unique(),
     revoked: boolean("revoked").default(false),
 
@@ -61,7 +61,7 @@ export const tokenTypeEnum = pgEnum("token_type_enum", [
 ]);
 
 export const oneTimeTokens = authSchema.table("one_time_tokens", {
-    id: uuid("id").primaryKey().default(sql`gen_random_uuid()`),
+    id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
     tokenType: tokenTypeEnum('token_type').notNull(),
     tokenHash: text("token_hash").notNull().unique(),
     revoked: boolean("revoked").default(false),
@@ -75,7 +75,7 @@ export const oneTimeTokens = authSchema.table("one_time_tokens", {
 export const userRoleEnum = pgEnum("user_role_enum", ["super_admin", "admin", "guest"]);
 
 export const roles = authSchema.table("roles", {
-    id: uuid("id").primaryKey().default(sql`gen_random_uuid()`),
+    id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
     name: userRoleEnum("name").notNull().unique(),
 
     createdAt: timestamp('created_at').notNull().defaultNow(),
