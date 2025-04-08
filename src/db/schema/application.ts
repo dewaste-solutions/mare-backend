c1edee01e48845d18a4f43f7f257cfa17907db4b
cr3wcr3w <racelisjk@gmail.com>
2025-04-08 23:02:43 +0800

    feat: expand application schema with new tables and relationships for requirements and answers

--- src/db/schema/application.ts
+++ src/db/schema/application.ts
@@ -1,16 +1,132 @@
 import { sql } from "drizzle-orm";
-import { pgSchema, timestamp, uuid } from "drizzle-orm/pg-core";
+import { boolean, pgSchema, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { oneTimeTokens, roles, users } from "./auth";
+import { statuses } from "./shared";
 
 export const applicationSchema = pgSchema("application");
 
 export const invitedUsers = applicationSchema.table("invited_users", {
 	id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
+	statusId: uuid("status_id")
+		.notNull()
+		.references(() => statuses.id, { onDelete: "set null" }),
+	oneTimeTokensId: uuid("one_time_tokens_id")
+		.notNull()
+		.references(() => oneTimeTokens.id, { onDelete: "cascade" }),
 	createdAt: timestamp("created_at").notNull().defaultNow(),
 	updatedAt: timestamp("updated_at").notNull(),
 });
 
 export const onBoarding = applicationSchema.table("onboarding", {
 	id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
+	statusId: uuid("status_id")
+		.notNull()
+		.references(() => statuses.id, { onDelete: "set null" }),
+	invitedUsersId: uuid("invited_users_id")
+		.notNull()
+		.references(() => invitedUsers.id, { onDelete: "cascade" }),
 	createdAt: timestamp("created_at").notNull().defaultNow(),
 	updatedAt: timestamp("updated_at").notNull(),
 });
+
+export const optionChoices = applicationSchema.table("option_choices", {
+	id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
+	requirementOptionId: uuid("requirement_option_id")
+		.notNull()
+		.references(() => requirementOptions.id, { onDelete: "cascade" }),
+	value: text("value").notNull(),
+	label: text("label").notNull(),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+	updatedAt: timestamp("updated_at").notNull(),
+});
+
+export const requirementOptions = applicationSchema.table(
+	"requirement_options",
+	{
+		id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
+		placeholder: text("placeholder").notNull(),
+		component: text("component").notNull(),
+		isRequired: boolean("is_required").notNull(),
+		defaultValue: text("default_value").notNull(),
+		createdAt: timestamp("created_at").notNull().defaultNow(),
+		updatedAt: timestamp("updated_at").notNull(),
+	},
+);
+
+export const requirements = applicationSchema.table("requirements", {
+	id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
+	requirementOptionsId: uuid("requirement_options_id")
+		.notNull()
+		.references(() => requirementOptions.id, { onDelete: "cascade" }),
+	name: text("name").notNull(),
+	description: text("description").notNull(),
+	roleId: uuid("role_id")
+		.notNull()
+		.references(() => roles.id, { onDelete: "set null" }),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+	updatedAt: timestamp("updated_at").notNull(),
+});
+
+export const requirementAnswers = applicationSchema.table(
+	"requirement_answers",
+	{
+		id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
+		requirementsId: uuid("requirement_id")
+			.notNull()
+			.references(() => requirements.id, { onDelete: "cascade" }),
+		answer: text("answer"),
+		createdAt: timestamp("created_at").notNull().defaultNow(),
+		updatedAt: timestamp("updated_at").notNull(),
+		statusId: uuid("status_id")
+			.notNull()
+			.references(() => statuses.id, { onDelete: "set null" }),
+		onBoardingId: uuid("onboarding_id")
+			.notNull()
+			.references(() => onBoarding.id, { onDelete: "cascade" }),
+	},
+);
+
+export const requirementFiles = applicationSchema.table("requirement_files", {
+	id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
+	requirementId: uuid("requirement_id")
+		.notNull()
+		.references(() => requirements.id, { onDelete: "cascade" }),
+	name: text("name").notNull(),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+	updatedAt: timestamp("updated_at").notNull(),
+});
+
+export const requirementAnswersFiles = applicationSchema.table(
+	"requirement_answers_files",
+	{
+		id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
+		requirementFileId: uuid("requirement_file_id")
+			.notNull()
+			.references(() => requirementFiles.id, { onDelete: "cascade" }),
+		statusId: uuid("status_id")
+			.notNull()
+			.references(() => statuses.id, { onDelete: "set null" }),
+		filePath: text("file_path").notNull(),
+		onBoardingId: uuid("onboarding_id")
+			.notNull()
+			.references(() => onBoarding.id, { onDelete: "cascade" }),
+		createdAt: timestamp("created_at").notNull().defaultNow(),
+		updatedAt: timestamp("updated_at").notNull(),
+	},
+);
+
+export const assignedVerifierConnection = applicationSchema.table(
+	"assigned_verifier_connection",
+	{
+		id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
+		userId: uuid("user_id")
+			.notNull()
+			.references(() => users.id, { onDelete: "cascade" }),
+		onBoardingId: uuid("onboarding_id")
+			.notNull()
+			.references(() => onBoarding.id, { onDelete: "cascade" }),
+		isSigned: boolean("is_signed").notNull(),
+		createdAt: timestamp("created_at").notNull().defaultNow(),
+		updatedAt: timestamp("updated_at").notNull(),
+	},
+);
