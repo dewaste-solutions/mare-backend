05939c7200330c607718490b71a13f72a6c2dd7a
tomdeocales <deocales8@gmail.com>
2025-04-13 21:37:02 +0800

    feat: add column for email - DWSDB-35

--- src/db/schema/application.ts
+++ src/db/schema/application.ts
@@ -1,10 +1,19 @@
 import { sql } from "drizzle-orm";
-import { pgSchema, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgSchema, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { oneTimeTokens } from "./auth";
+import { statuses } from "./shared";
 
 export const applicationSchema = pgSchema("application");
 
 export const invitedUsers = applicationSchema.table("invited_users", {
 	id: uuid("id").notNull().primaryKey().default(sql`gen_random_uuid()`),
+	statusId: uuid("status_id")
+		.notNull()
+		.references(() => statuses.id, { onDelete: "set null" }),
+	oneTimeTokensId: uuid("one_time_tokens_id")
+		.notNull()
+		.references(() => oneTimeTokens.id, { onDelete: "cascade" }),
+	email: text("email").notNull().unique(),
 	createdAt: timestamp("created_at").notNull().defaultNow(),
 	updatedAt: timestamp("updated_at").notNull(),
 });
