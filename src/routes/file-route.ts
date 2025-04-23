79ed4f5a57beb772e506b7740a48f8292620c4b7
Tom <deocales8@gmail.com>
2025-04-24 00:09:45 +0800

    feat: add route for deleting file

---
+++ src/routes/file-route.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { uploadFile } from "../controller/file-upload/upload-file";
+import { deleteFile } from "../controller/file-upload/delete-file";
+
+export const fileRoutes = express.Router();
+
+// File upload route - single file upload
+fileRoutes.post("/upload", uploadFile);
+
+// File delete route - using * to capture the entire path including slashes and encoded characters
+fileRoutes.delete("/*", deleteFile);
