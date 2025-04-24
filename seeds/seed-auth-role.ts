db255198340914e6ae1b734a105973d8db99ce8d
tomdeocales <deocales8@gmail.com>
2025-04-24 19:15:53 +0800

    feat: add a new permission to admin, update:status - DWSDB-38

--- seeds/seed-auth-role.ts
+++ seeds/seed-auth-role.ts
@@ -43,6 +43,11 @@ export async function seedAuthRole() {
 				scope: "read:roles",
 				updatedAt: sql`NOW()`,
 			},
+			{
+				description: "Can update status",
+				scope: "update:status",
+				updatedAt: sql`NOW()`,
+			},
 		]);
 
 		const insertedPermissions = await tx.select().from(permissions);
@@ -64,7 +69,7 @@ export async function seedAuthRole() {
 
 		// Assign admin-only permission
 		if (roleMap.admin) {
-			const adminOnlyPermissions = ["create:invitation", "read:roles"];
+			const adminOnlyPermissions = ["create:invitation", "read:roles", "update:status"];
 
 			const adminRolePermissions = adminOnlyPermissions.map((scope) => ({
 				roleId: roleMap.admin,
