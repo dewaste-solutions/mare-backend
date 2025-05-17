77232e99f8bd63f37419e76d54750f3e3694be81
cr3wcr3w <racelisjk@gmail.com>
2025-05-18 07:53:06 +0800

    fix: update CORS configuration to use environment variable for frontend URL

--- src/index.ts
+++ src/index.ts
@@ -12,15 +12,14 @@ import { sharedRoute } from "./routes/shared-route";
 
 const app = express();
 const PORT = env.BACKEND_PORT;
-// app.use(
-// 	cors({
-// 	  origin: "http://localhost.com", // Replace with your frontend's origin
-// 	  methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
-// 	  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-// 	})
-// );
+app.use(
+	cors({
+		origin: env.BACKEND_FRONTEND_URL,
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
+	}),
+);
 
-app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(httpLogger());
