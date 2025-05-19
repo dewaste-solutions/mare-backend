ce1428ab5d9fcac34bf645b0f6a42de49ee5725d
cr3wcr3w <racelisjk@gmail.com>
2025-05-19 22:12:54 +0800

    feat: add create question by role functionality
    - Implemented a new endpoint to create questions based on user roles.
    - Added validation for the request body using Zod.
    - Updated database schema to remove unique constraints on requirement categories and sections.
    - Created SQL migration for dropping unique constraints.
    - Updated application routes to include the new endpoint.
    - Added transaction handling for inserting questions, sections, and categories into the database.

--- src/middleware/application/validate-body.ts
+++ src/middleware/application/validate-body.ts
@@ -24,3 +24,56 @@ export function validateApplicationSubmit(
 
 	next();
 }
+
+const QuestionSchema = z.object({
+	categoryName: z.string(),
+	requirementStep: z.number(),
+	sections: z.array(
+		z.object({
+			sectionName: z.string(),
+			sectionOrder: z.number(),
+			questions: z.array(
+				z.object({
+					question: z.string(),
+					description: z.string(),
+					isRequired: z.boolean(),
+					placeholder: z.string(),
+					defaultValue: z.string(),
+					component: z.enum([
+						"radiogroup",
+						"input_text",
+						"input_email",
+						"textarea",
+						"date",
+						"select_upload",
+					]),
+					order: z.string(),
+					allowMultiple: z.boolean(),
+					choices: z.array(
+						z.object({
+							choicesName: z.string(),
+						}),
+					),
+				}),
+			),
+		}),
+	),
+});
+
+const bodySchemaCreateQuestion = z.object({
+	answers: z.array(QuestionSchema),
+});
+
+export function validateCreateQuestionByRole(
+	req: Request,
+	_res: Response,
+	next: NextFunction,
+): void {
+	const result = bodySchemaCreateQuestion.safeParse(req.body);
+	if (!result.success) {
+		const message = `Zod :: ${result.error.errors[0].message} :: ${result.error.errors[0].path}`;
+		next(message);
+	}
+
+	next();
+}
